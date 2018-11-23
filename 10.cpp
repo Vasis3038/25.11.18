6931737166bc5203c16c643d559060ca37185c46
Vasis3038 <43793237+Vasis3038@users.noreply.github.com>
2018-11-24 00:05:26 +0300

    Create 10.cpp

---
+++ 10.cpp
@@ -0,0 +1,24 @@
+# 25.11.18
+//Напишите функцию вычисляющую праймориал числа. Программа должна работать не более чем за O(n*log log n) шагов.
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ int n;
+ cin >> n;
+ bool A[n];
+ long long k = 1;
+ for (int i = 2; i<n; i++){
+        if (i*i>n) break;
+    for(long g = i*i; g<=n; g = g+i){
+        A[g]=1;
+    }
+ }
+    for (int i=2; i<=n; i++){
+        if (A[i] == 0){k = k*i ;}
+    }
+    cout << k;
+    return 0;
+}
