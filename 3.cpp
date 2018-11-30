c183b021d9e9e5b81bcad5004b750b038ea53e6f
Vasis3038 <43793237+Vasis3038@users.noreply.github.com>
2018-11-30 22:37:24 +0300

    Create 3.cpp

---
+++ 3.cpp
@@ -0,0 +1,35 @@
+//Напишите переборную программу программу, вычисляющую значение функции Эйлера для числа n. Программа должна работать не более чем за O(n* log n) шагов.
+
+#include <iostream>
+
+using namespace std;
+
+Euler int (int n){
+    int p = 1;
+    int ans = 1;
+    for (int i = 2; i<= n;){
+        if (n%i == 0){
+                if(i == p){ ans= ans*p;
+                else
+                {
+                ans = ans*(i - 1);
+                p = i;
+                }
+        n = n / i;
+        }
+        else
+        {
+            i++;}
+
+    }
+    return ans;
+
+
+}
+int main()
+{
+    int n;
+    cin >> n;
+    cout << Euler(n);
+    return 0;
+}
