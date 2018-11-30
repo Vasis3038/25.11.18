607a0e5a434306e18ad5e0942e41622a25a270ae
Vasis3038 <43793237+Vasis3038@users.noreply.github.com>
2018-11-30 16:42:59 +0300

    Update 10.cpp

--- 10.cpp
+++ 10.cpp
@@ -4,10 +4,8 @@
 
 using namespace std;
 
-int main()
+int priori(int n)
 {
- int n;
- cin >> n;
  bool A[n];
  long long k = 1;
  for (int i = 2; i<n; i++){
@@ -19,6 +17,15 @@ int main()
     for (int i=2; i<=n; i++){
         if (A[i] == 0){k = k*i ;}
     }
-    cout << k;
+ return k;
+}
+
+
+
+int main()
+{
+ int n;
+ cin >> n;
+    cout << priori(n);
     return 0;
 }
